2166b41a43f9bc4b97465922a6036f5c8b1eb0f4
BasitAyaz <basitahmed1997@gmail.com>
2025-06-20 08:42:42 -0700

    Class Code

---
+++ app.js
@@ -0,0 +1,372 @@
+// alert("Bismillah")
+// window.alert("Alhamdulillah")
+
+// var a = "Abdul Basit"
+// alert(a)
+// console.log(a)
+
+// var x = 10
+// var y = 20
+// var z = 5
+// var b = 30
+
+// console.log(x + (y - z) * b)
+
+// var a correct 
+// var a1 correct
+// var 1a incorrect
+// var console incorrect
+// var a-1  incorrect
+// var a 1  incorrect
+// var a_1 correct
+// var (asd) incorrect
+
+
+
+// var firstName = "Basit" //string
+// var lastName = "Ahmed" //string
+// var qualified = false //boolean 
+// var age = 12 //number
+// var shadiKabHogi = undefined 
+// var tumharaApnaKiaHai = null 
+// var hobbies = [] //object
+
+
+// var a = 10
+// console.log(a)
+
+// a = 20
+// console.log(a)
+
+
+// var b = "ABC"
+// console.log(b)
+
+// b = "XYZ"
+// console.log(b)
+
+
+// let a = 10;
+// console.log(a)
+
+// a = 20
+// console.log(a)
+
+
+// var a = 10;
+// console.log(a)
+
+
+// var a = 20;
+// console.log(a)
+
+
+// let a = 10
+// console.log(a)
+
+
+// let a = 20
+// console.log(a)
+
+// var b;
+// console.log(b)
+
+// b = 10
+// console.log(b)
+
+// let a
+// console.log(a)
+
+// a = 10
+// console.log(a)
+
+// const a = 10
+// console.log(a)
+
+// a = 20
+// console.log(a)
+
+// let firstName = "Basit"
+// let lastName = "Ahmed"
+
+// let fullName = firstName + " " + lastName
+// console.log(fullName)
+
+
+
+// let a = "10b"
+// let b = "20a"
+// skldjs
+
+// console.log(b-a)
+
+// let a = 10
+
+// // a++
+// // a--
+
+// let b = a++
+
+// console.log(b)
+// console.log(a)
+
+// let a = 5
+
+// let b = ++a
+
+// console.log(b)
+
+// let firstName = +prompt("enter first name")
+// console.log(firstName)
+
+
+// take data from user of firstName, lastName, email, contact
+
+// take marks of 5 random subjects and calculate percentage 
+// (total marks of all subject is 500)
+
+// step 1 = summ all marks and store in a variable
+// step 2 = create a new variable and calculate percentage in it.
+// step 3 = show percentage in console.
+
+// take Product Price and apply 20% discount on the product value.
+
+// let age = 20
+
+// if(age == 18){}
+// if(age != 18){}
+// if(age > 18){}
+// if(age < 18){}
+// if(age >= 18){}
+// if(age <= 18){}
+
+// if (age >= 18) {
+//     alert("Student Allowed")
+// }
+
+// let age = +prompt("Enter Your Age")
+
+// if(age === 18){
+//     console.log("Allowed")
+// }
+
+
+// if('10' === '10'){
+//     console.log("Equal")
+// }
+
+// let age = 12
+// let studentCard = true
+
+// if (age >= 18) {
+//     console.log("Allowed")
+// } else if (studentCard === true) {
+//     console.log("Allowed")
+// } else {
+//     console.log("Not Allowed")
+// }
+
+// let a  = 20
+
+// if(a === 20){
+//     console.log("Correct")
+//     let b = 30
+// }
+
+// console.log(b)
+
+
+// let age = 18
+// let studentCard = true
+
+// if(age >= 18 ){
+//     if(studentCard == true){
+//         console.log("Allow")
+//     }
+// }
+
+// console.log(age >= 18)
+
+// if(age >= 18 || studentCard){
+//     console.log("Allow")
+// }
+
+
+
+// take marks using prompt and show grade
+
+// grade A > 91 <===> 100
+// grade B > 81 <===> 90
+// grade C > 71 <===> 80
+// grade D > 61 <===> 70
+
+// let age = 18
+
+// if(age >= 18){
+//     console.log("Allowed")
+// }else{
+//     console.log("Not Allowed")
+// }
+
+// age >= 18 ? console.log("Allowed") : console.log("Not Allowed")
+
+
+// let allowed
+// let studentCard = false
+
+// studentCard ? allowed = true : allowed = false
+
+// let fullName = "Bast Bin Ayaz"
+
+// let b = "My Name is " + fullName + " my profession is Software Development"
+
+// let a = `My Name is ${fullName} my profession is Software Development`
+
+// console.log(a)
+// console.log(b)
+
+
+// let studentName = "Basit Bin Ayaz"
+// let studentCard = false
+
+
+
+// let a = `My name is ${studentName} and Student ${studentCard ? 'Allowed' : 'Not Allowed'} on Behalf of Student Card`
+
+
+// let studentNames = [
+//     'Abdullah',
+//     'Muhammad Ahmed',
+//     'Abu Bakar',
+//     'Muhammad Umar',
+//     'Muhammad Usman',
+//     'Muhammad ALi',
+//     'Muhammad Hasan',
+//     'Muhammad Hussain',
+//     'Fatima',
+//     'Hamza'
+// ]
+
+// console.log(studentNames[2])
+// console.log(studentNames.length)
+
+// studentNames[9] = "Ameer Muawiya"
+// console.log(studentNames)
+
+// studentNames.push("Talha bin Zubair")
+// console.log(studentNames)
+
+// studentNames.pop()
+// console.log(studentNames)
+
+// studentNames.shift()
+// studentNames.unshift("Value")
+
+// let a = studentNames.slice(2,5)
+// let a = studentNames.slice(2)
+// console.log(a)
+// console.log(studentNames)
+
+// studentNames.splice(8,1)
+// studentNames.splice(2,3)
+// console.log(studentNames)
+
+// let studentNames = [
+//     'Abdullah',
+//     'Muhammad Ahmed',
+//     'Abu Bakar',
+//     'Muhammad Umar',
+//     'Muhammad Usman',
+//     'Muhammad ALi',
+//     'Muhammad Hasan',
+//     'Muhammad Hussain',
+//     'Fatima',
+//     'Hamza'
+// ]
+
+// console.log(studentNames[2])
+
+// let i = studentNames.indexOf('Khadeeja')
+// console.log(i)
+
+// let exist = studentNames.includes('Muhammad Hasan')
+// console.log(exist)
+
+
+// let a = [[1,2,3,4],['a','b','c','d'],[true,false]]
+
+
+// console.log(a[1][2])
+// let b = a.flat()
+// console.log(b);
+
+// for (let i = 0; i < 10; i++) { 
+//     console.log("For Loop")
+// }
+
+// for (let i = 0; i < 10; i++) { 
+//     console.log(i)
+// }
+
+// let studentNames = [
+//     'Abdullah',
+//     'Muhammad Ahmed',
+//     'Abu Bakar',
+//     'Muhammad Umar',
+//     'Muhammad Usman',
+//     'Muhammad ALi',
+//     'Muhammad Hasan',
+//     'Muhammad Hussain',
+//     'Fatima',
+//     'Hamza'
+// ]
+
+// let a = "Abu Bakar"
+// let check = false
+
+// for (let i = 0; i < studentNames.length; i++) {
+//     // console.log(studentNames[i])
+
+//     if(a === studentNames[i]){
+//         check = true
+//         break
+//     }
+// }
+
+// if(check){
+//     console.log("Name Found")
+// }else{
+//     console.log("Name Not Found")
+// }
+
+// for (let i = 0; i < 10; i++) {
+//     console.log(`Outer Loop ${i}`)
+//     for (let j = 0; j < 5; j++) {
+//         console.log(`Inner Loop ${j}`)
+//     }
+// }
+
+// let a = ['a', 'b', 'c', 'd', 'e']
+// let b = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+
+// output should be like
+// ['1a','1b','1c','1d','1e','2a','2b'............. '9e']
+
+// let c = []
+// for(let i = 0; i < b.length; i++){
+//     for(let j = 0; j<a.length; j++){
+//         let str = b[i] + a[j]
+//         console.log(str)
+//         c.push(str)
+//     }
+// }
+// console.log(c)
+
+let a = "karachi"
+let b = prompt("Enter Your City")
+b = b.toLowerCase()
+
+if(a == b){
+    console.log("City Matched")
+}
+
